d04f8dac4587d8985da49954d72cadf5cbe1bf37
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-08-29 15:31:35 -0300

    chore: Projeto inicializado

---
+++ src/main/java/com/noirloom/noirloom/NoirloomApplication.java
@@ -0,0 +1,11 @@
+package com.noirloom.noirloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoirloomApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NoirloomApplication.class, args);
+	}
+}
---
+++ src/test/java/com/noirloom/noirloom/NoirloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noirloom.noirloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoirloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1c62f334f245ecc736f981bd06d4aa0567636d47
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-09-09 08:29:06 -0300

    feat: Criados os services e controllers das rotas de Itens de listar por id, criar, deletar e atualizar

--- src/main/java/com/noirloom/noirloom/controllers/ItemController.java
+++ src/main/java/com/noirloom/noirloom/controllers/ItemController.java
@@ -5,9 +5,7 @@ import com.noirloom.noirloom.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,19 @@ public class ItemController {
 
     @GetMapping("/list")
     public ResponseEntity<List<ItemDto>> getItems() { return itemService.listItems(); }
+
+    @GetMapping("/listById/{id}")
+    public ResponseEntity<ItemDto> getItem(@PathVariable Long id) { return itemService.listItemById(id) ; }
+
+    @PostMapping("/create")
+    public ResponseEntity<ItemDto> createItem(@RequestBody ItemDto data) { return itemService.createItem(data) ; }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteItem(@PathVariable Long id) { return itemService.deleteItem(id) ; }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<ItemDto> updateItem(@PathVariable Long id, @RequestBody ItemDto data) throws Exception {
+        return itemService.updateItem(data, id);
+    }
 }
+
--- src/main/java/com/noirloom/noirloom/services/ItemService.java
+++ src/main/java/com/noirloom/noirloom/services/ItemService.java
@@ -6,11 +6,15 @@ import com.noirloom.noirloom.models.ItemModel;
 import com.noirloom.noirloom.repositories.ItemRepository;
 import jakarta.transaction.Transactional;
 import org.apache.coyote.Response;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -28,4 +32,42 @@ public class ItemService {
         return ResponseEntity.ok(items);
     }
 
+    @Transactional
+    public ResponseEntity<ItemDto> listItemById(Long id) {
+        Optional<ItemModel> itemEntity = itemRepository.findById(id);
+        if (itemEntity.isEmpty()) {return ResponseEntity.notFound().build();}
+
+        ItemDto item = ItemMapper.mapToItemDTO(itemEntity.get());
+        return ResponseEntity.ok(item);
+    }
+
+    @Transactional
+    public ResponseEntity<ItemDto> createItem(ItemDto data) {
+        ItemModel newItem = ItemMapper.mapToItemModel(data);
+        itemRepository.save(newItem);
+
+        ItemDto newItemDto = ItemMapper.mapToItemDTO((newItem));
+        return ResponseEntity.ok(newItemDto);
+    }
+
+    public ResponseEntity<Void> deleteItem(Long id) {
+        Optional<ItemModel> item = itemRepository.findById(id);
+        if (item.isEmpty()) return ResponseEntity.notFound().build();
+
+        itemRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @Transactional
+    public ResponseEntity<ItemDto> updateItem(ItemDto data, Long id) throws Exception{
+        ItemModel itemEntity = itemRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Item not found"));
+
+        ItemModel updatedData = ItemMapper.mapToItemModel(data);
+        BeanUtils.copyProperties(updatedData, itemEntity, "id");
+
+        ItemModel updatedTask = itemRepository.save(itemEntity);
+        ItemDto uptadedItemDto = ItemMapper.mapToItemDTO(updatedTask);
+        return ResponseEntity.ok(uptadedItemDto);
+    }
 }

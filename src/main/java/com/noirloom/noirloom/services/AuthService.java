27ea8dd29f8d8888549565679d49b220c7758c14
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-09-12 11:24:26 -0300

    feat: Created a method in AuthService that loads an user found through an e-mail

--- src/main/java/com/noirloom/noirloom/services/AuthService.java
+++ src/main/java/com/noirloom/noirloom/services/AuthService.java
@@ -1,15 +1,22 @@
 package com.noirloom.noirloom.services;
 
-import com.noirloom.noirloom.DTOs.UserDto;
-import org.springframework.http.ResponseEntity;
+import com.noirloom.noirloom.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-/*
+
 @Service
-public class AuthService {
+public class AuthService implements UserDetailsService {
 
-    public ResponseEntity<Void> register(UserDto data) {
+    @Autowired
+    UserRepository userRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
     }
+
 }
- */

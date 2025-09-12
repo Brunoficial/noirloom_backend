73ed7b79cdaeb26ec5519f27d5abf310201ee29f
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-09-12 11:21:18 -0300

    feat: Added method that searchs for an user through the e-mail

--- src/main/java/com/noirloom/noirloom/repositories/UserRepository.java
+++ src/main/java/com/noirloom/noirloom/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.noirloom.noirloom.repositories;
 
 import com.noirloom.noirloom.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+    UserDetails findByEmail(String email);
 }

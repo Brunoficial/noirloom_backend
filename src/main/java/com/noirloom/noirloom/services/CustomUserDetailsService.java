e5d0b4891972c83d66aad61f168db42ee37e41c9
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-09-30 10:28:24 -0300

    fix: Created a new file to handle UserDetailsService responsabilities, giving that a cycle of dependencies was happening

---
+++ src/main/java/com/noirloom/noirloom/services/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.noirloom.noirloom.services;
+
+import com.noirloom.noirloom.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByEmail(username);
+    }
+}

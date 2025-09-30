62e14d7804e7906a1f56b697a7f9a69dab98b3d9
Brunoficial <bruno.lopes.vrls@gmail.com>
2025-09-30 10:26:45 -0300

    feat: Defined the views of login and register, creating a new cart entity as soon as an user is created

--- src/main/java/com/noirloom/noirloom/services/AuthService.java
+++ src/main/java/com/noirloom/noirloom/services/AuthService.java
@@ -1,22 +1,66 @@
 package com.noirloom.noirloom.services;
 
+import com.noirloom.noirloom.DTOs.AuthDto;
+import com.noirloom.noirloom.DTOs.LoginResponseDto;
+import com.noirloom.noirloom.DTOs.RegisterDto;
+import com.noirloom.noirloom.infra.security.TokenService;
+import com.noirloom.noirloom.models.CartModel;
+import com.noirloom.noirloom.models.UserModel;
+import com.noirloom.noirloom.repositories.CartRepository;
 import com.noirloom.noirloom.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.management.remote.JMXAuthenticator;
 
 
 @Service
-public class AuthService implements UserDetailsService {
+public class AuthService {
 
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByEmail(username);
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public ResponseEntity register(RegisterDto data) {
+        if (this.userRepository.findByEmail(data.getEmail()) != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.getPassword());
+        UserModel newUser = new UserModel(data.getName() ,data.getEmail(), encryptedPassword, data.getRole());
+
+        this.userRepository.save(newUser);
+
+        CartModel newCart = new CartModel();
+        newCart.setUser(newUser);
+
+        this.cartRepository.save(newCart);
+
+        return ResponseEntity.ok().build();
     }
 
+    public ResponseEntity login (AuthDto data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.getEmail(), data.getPassword());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((UserModel) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginResponseDto(token));
+    }
 }
